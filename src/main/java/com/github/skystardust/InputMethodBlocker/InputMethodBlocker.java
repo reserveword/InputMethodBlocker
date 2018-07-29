98975454fb3232b38651a25e4f21107d5ba23f67
TimmyQAQ <skystardust@qq.com>
2018-07-29 01:24:21 -0700

    提高用户体验

--- src/main/java/com/github/skystardust/InputMethodBlocker/InputMethodBlocker.java
+++ src/main/java/com/github/skystardust/InputMethodBlocker/InputMethodBlocker.java
@@ -1,9 +1,18 @@
 package com.github.skystardust.inputmethodblocker;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.client.gui.inventory.GuiEditSign;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,6 +23,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 
 @Mod(
         modid = InputMethodBlocker.MOD_ID,
@@ -38,19 +48,23 @@ public class InputMethodBlocker {
         switchIMEKey = new KeyBinding("切换IME", Keyboard.KEY_P, "InputMethodBlocker");
         ClientRegistry.registerKeyBinding(switchIMEKey);
         MinecraftForge.EVENT_BUS.register(this);
-        System.out.println(IMENativeAccess.getIMEStatus());
-        IMENativeAccess.inactiveIME();
-        System.out.println(IMENativeAccess.getIMEStatus());
+        FMLCommonHandler.instance().bus().register(this);
     }
 
     @SubscribeEvent
     public void onPlayerKeyDown(InputEvent.KeyInputEvent keyboardInputEvent) {
+        EntityPlayerSP player = Minecraft.getMinecraft().player;
+        if (player == null) {
+            return;
+        }
         if (switchIMEKey.isPressed()) {
-            System.out.println(IMENativeAccess.getIMEStatus());
+
             if (IMENativeAccess.getIMEStatus()) {
                 IMENativeAccess.inactiveIME();
+                sendMessage("取消激活输入法");
             } else {
                 IMENativeAccess.activeIME();
+                sendMessage("激活输入法");
             }
         }
     }
@@ -61,11 +75,48 @@ public class InputMethodBlocker {
             if (IMENativeAccess.getIMEStatus()) {
                 IMENativeAccess.inactiveIME();
             }
-            return;
         }
-        if (!IMENativeAccess.getIMEStatus()) {
+        if (guiOpenEvent.getGui() instanceof GuiEditSign) {
             IMENativeAccess.activeIME();
         }
+    }
+
+    @SubscribeEvent
+    public void onMouseClick(GuiScreenEvent.MouseInputEvent guiScreenEvent){
+        detectGui(guiScreenEvent.getGui());
+    }
+
+    private void detectGui(GuiScreen gui){
+        if (gui.allowUserInput) {
+            IMENativeAccess.activeIME();
+        }
+        for (Field field : gui.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            try {
+                Object o = field.get(gui);
+                if (o != null) {
+                    if (o instanceof GuiTextField) {
+                        if (((GuiTextField) o).isFocused()) {
+                            IMENativeAccess.activeIME();
+                            return;
+                        }
+                    }
+                    IMENativeAccess.inactiveIME();
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sendMessage(String string){
+        EntityPlayerSP player = Minecraft.getMinecraft().player;
+        if (player == null) {
+            return;
+        }
+        player.sendMessage(ITextComponent.Serializer.jsonToComponent("{\n" +
+                "    \"text\": \""+string+"\"\n" +
+                "}"));
     }
 
     public void saveNativeFile() throws IOException {
